b0443c297ccbe86c2e9e5fd881acfe065b0cc70e
Karol <libakary@users.noreply.github.com>
2023-04-20 14:58:05 +0300

    font replaced

--- TARge21Estpank/TARge21Estpank/MauiProgram.cs
+++ TARge21Estpank/TARge21Estpank/MauiProgram.cs
@@ -9,8 +9,9 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				//fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				//fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("scribble-box-demo.ttf", "ScribbleBox");
 			});
 
 		return builder.Build();
