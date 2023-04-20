b073315a166b22a44cb5ab766b7d1f72342bef7d
Karol <libakary@users.noreply.github.com>
2023-04-20 14:43:01 +0300

    1

--- TARge21Estpank/TARge21Estpank/App.xaml.cs
+++ TARge21Estpank/TARge21Estpank/App.xaml.cs
@@ -1,10 +1,33 @@
-﻿namespace TARge21Estpank;
+﻿#if WINDOWS
+using Microsoft.UI;
+using Microsoft.UI.Windowing;
+//using TARge21Estpank.Views;
+using Windows.Graphics;
+#endif
+
+namespace TARge21Estpank;
 
 public partial class App : Application
 {
+	const int WindowWidth = 540;
+	const int WindowHeight = 1400;
+
 	public App()
 	{
 		InitializeComponent();
+
+#if WINDOWS
+		Microsoft.Maui.Handlers.WindowHandler.Mapper.AppendToMapping(nameof(IWindow), (handler, view) =>
+		{
+			var mauiWindow = handler.VirtualView;
+			var nativeWindow = handler.PlatformView;
+			nativeWindow.Activate();
+			IntPtr windowhandle = WinRT.Interop.WindowNative.GetWindowHandle(nativeWindow);
+			WindowId windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(windowhandle);
+			AppWindow appWindow = Microsoft.UI.Windowing.AppWindow.GetFromWindowId(windowId);
+			appWindow.Resize(new SizeInt32(WindowWidth, WindowHeight));
+		});
+#endif
 
 		MainPage = new AppShell();
 	}
