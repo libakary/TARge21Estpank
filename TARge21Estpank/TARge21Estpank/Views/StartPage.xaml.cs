961a32200c027ccc477ade46450d9beda4be291d
Emma :) <99496596+kodukass@users.noreply.github.com>
2023-04-27 15:38:15 +0300

    aight tahaks mingeid detaile lisada aga photoshop ei tööta :,)

--- TARge21Estpank/TARge21Estpank/Views/StartPage.xaml.cs
+++ TARge21Estpank/TARge21Estpank/Views/StartPage.xaml.cs
@@ -8,5 +8,5 @@ public partial class StartPage : ContentPage
 	}
 
 	async void Sisene_Clicked(System.Object sender, System.EventArgs e)
-		=> Application.Current.MainPage = new NavigationPage(new BalancePage()); //LoginPage
+		=> Application.Current.MainPage = new NavigationPage(new LoginPage()); //LoginPage
 }
