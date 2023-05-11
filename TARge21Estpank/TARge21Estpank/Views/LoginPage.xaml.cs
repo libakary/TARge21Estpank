c51d0eec4fdebb8d68c8a4561726ca585b2c421c
Karol <libakary@users.noreply.github.com>
2023-05-11 14:55:17 +0300

    tried/failed to add admin entry only

--- TARge21Estpank/TARge21Estpank/Views/LoginPage.xaml.cs
+++ TARge21Estpank/TARge21Estpank/Views/LoginPage.xaml.cs
@@ -2,9 +2,14 @@ namespace TARge21Estpank.Views
 {
     public partial class LoginPage : ContentPage
     {
-        public LoginPage()
+		//public string adminUsername { get; set; }
+		//public int adminPassword { get; set; }
+		public LoginPage()
         {
             InitializeComponent();
+
+            //adminUsername = "admin";
+            //adminPassword = 1234;
         }
 
         private async void Sisene_Clicked(object sender, EventArgs e)
@@ -14,6 +19,10 @@ namespace TARge21Estpank.Views
                 await Navigation.PushAsync(new BalancePage(UsernameEntry.Text));
                 await DisplayAlert("Õnnestus", $"Tere, {UsernameEntry.Text}!", "Edasi");
             }
+            //else ((UsernameEntry.ToString != adminUsername.ToString) && (PasswordEntry.ToString != adminPassword.ToString))
+            //{
+
+            //}
             else
             {
                 await DisplayAlert("Viga", "Proovi uuesti, äkki veab.", "Tagasi");
